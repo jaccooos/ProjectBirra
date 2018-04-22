c459d36109da67ebabcbc76918413d0f316a19f7
jaccooos <38593287+jaccooos@users.noreply.github.com>
2018-04-22 21:14:12 +0200

    Add files via upload

--- ports2560.h
+++ ports2560.h
@@ -1,16 +1,16 @@
 /*
-*  ports328.h
+*  ports2560.h
 *
-*  Copyright (c) 2011 by Avans Hogeschool.
+*  
 * 
-*  Date:    26-mar-2012
+*  Date:    22-mar-2012
 *
-*  Author:  Gerbrandt Schor
+*  Author:  J.C. Oostdijk
 */
 
 
-#ifndef PORTS328_H
-#define PORTS328_H
+#ifndef PORTS2560_H
+#define PORTS2560_H
 
 
 ///////////////////////////////////////////////////////////////////////////////
@@ -18,4 +18,32 @@
 
 void initPorts(void);
 
-#endif //PORTS328_H
+/* 
+
+
+de output pinnen voor de stepper motoren zijn: 
+1_STEP_PIN         54
+1_DIR_PIN          55
+1_ENABLE_PIN       38
+
+2_STEP_PIN         60
+2_DIR_PIN          61
+2_ENABLE_PIN       56
+
+
+3_STEP_PIN         46
+3_DIR_PIN          48
+3_ENABLE_PIN       62
+
+4_STEP_PIN         26
+4_DIR_PIN          28
+4_ENABLE_PIN       24
+
+5_STEP_PIN         36
+5_DIR_PIN          34
+5_ENABLE_PIN       30
+
+*/
+
+
+#endif //PORTS2560_H
