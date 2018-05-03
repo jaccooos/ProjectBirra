9ac70f3dca5dc4b4e6385addd9372a304ca826e4
Damian1024 <38593668+Damian1024@users.noreply.github.com>
2018-05-03 12:15:15 +0200

    Add files via upload

---
+++ ProcessenTapbier.c
@@ -0,0 +1,134 @@
+/*
+*  Processen.c
+*
+*
+*  Date:    28-04-2018
+*
+*  Author:
+*/
+
+
+///////////////////////////////////////////////////////////////////////////////
+// system includes
+
+#include <inttypes.h>
+#include <avr/io.h>
+#include <stdbool.h>
+#include <avr/interrupt.h>
+
+
+///////////////////////////////////////////////////////////////////////////////
+// application specific includes
+
+#include "ports2560.h"
+#include "Mainheader.h"
+#include "Serial.h"
+#include "Timer.h"
+#include "Testrun.h"
+#include "Stepper.h"
+
+///////////////////////////////////////////////////////////////////////////////
+// file globals
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// LaatBekerVallen
+
+bool LaatBekerVallen(void)
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// TapBiertje
+
+bool TapBiertje (void)
+{
+    bool BekerAanwezig    = false;
+    bool TappenKlaar      = false;
+    bool TapBiertje       = false;
+    bool WachtOpTap       = false;
+    bool Bekerdoorgeven   = false;
+    uint8_t WaterklepOpen;
+    uint8_t WaterklepDicht;
+
+    while (true)
+    {
+        if (BekerAanwezig == true)         // wachten tot beker aanwezig is
+        {
+             if (Tapbiertje == false)      // biertje is niet aan het tappen
+             {
+                 if  (TappenKlaar == true) // tappen is klaar
+                    {
+                    if (Bekerdoorgeven == false)   //bekerdoorgeven is klaar controle
+                    {
+                        while (TapAan & BekerIsNietVol)
+                        {
+                            WaterklepOpen;
+                            {
+                                if (flowmeter 300ml)
+                                {
+                                    WaterklepDicht;
+                                    Tappenklaar = true;
+                                }
+                            }
+
+                        }
+
+
+                    }
+                    else (Bekerdoorgeven == true)
+                    {
+                        Delay()
+                    }
+                 }
+                 else (TappenKlaar == false)
+                 {
+                      ErrorTappen();
+                 }
+            }
+            else (TapBiertje == true)
+            {
+                ErrorBiertjeAanHetTappen();
+            }
+        }
+        else (BekerAanwezig == false)
+        {
+            ErrorBekerafwezig();
+        }
+}
+
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// DraaiTafel
+
+bool DraaiTafel(void)
+{
+
+}
+
+///////////////////////////////////////////////////////////////////////////////
+// OpenDeur
+
+
+
+
+
+///////////////////////////////////////////////////////////////////////////////
+// DichtDeur
+
+bool DichtDeur(void)
+{
+
+}
+
+
+
