b4ccf9a93acce6bf448f54effded576fef8801e9
jaccooos <38593287+jaccooos@users.noreply.github.com>
2018-05-07 09:27:00 +0200

    processen samengevoegd

--- Mainheader.h
+++ Mainheader.h
@@ -12,16 +12,22 @@
 #ifndef Mainheader_H
 #define Mainheader_H
 
-unsigned char Done 			='d';
-unsigned char Uitstoten 	='u';
-unsigned char Tappen 		='t';
-unsigned char Draaien 		='g';
-unsigned char Received 	 	='r';
-unsigned char Bestelling 	='b';
-unsigned char Opstarten 	='o';
-unsigned char Error		 	='e';
-unsigned char Standby	 	='s';
+
+unsigned char Uitstoten 	='s0\n'; // 0
+unsigned char Tappen 		='s1\n'; // 1
+unsigned char Draaien 		='s2\n'; // 2
+unsigned char DeurOpenen	='s3\n'; // 3
+unsigned char Draaien 		='s4\n'; // 4
+unsigned char Standby	 	='s5\n'; // 5
+unsigned char Opstarten 	='s6\n'; // 6
+
+unsigned char Done 			='d\n';
+unsigned char Received 	 	='r\n'; 
+unsigned char Bestelling 	='b\n';
+unsigned char Error		 	='e\n';
 unsigned char Temperatuur 	='c';
+#define Vooraad 			='v';
+#define Magazijn			='m';
 
 //  data sturen is via "letter" dan "eventueel getal" en dan "/n"
 
--- Processen.c
+++ Processen.c
@@ -46,9 +46,61 @@ bool LaatBekerVallen(void)
 ///////////////////////////////////////////////////////////////////////////////
 // TapBiertje
 
+
 bool TapBiertje (void)
 {
+    bool BekerAanwezig    = false;
+    bool TappenKlaar      = false;
+    bool TapBiertje       = false;
+    bool WachtOpTap       = false;
+    bool Bekerdoorgeven   = false;
+    uint8_t WaterklepOpen;
+    uint8_t WaterklepDicht;
 
+    while (true)
+    {
+        if (BekerAanwezig == true)         // wachten tot beker aanwezig is
+        {
+             if (Tapbiertje == false)      // biertje is niet aan het tappen
+             {
+                 if  (TappenKlaar == true) // tappen is klaar
+                    {
+                    if (Bekerdoorgeven == false)   //bekerdoorgeven is klaar controle
+                    {
+                        while (TapAan & BekerIsNietVol)   //Tap moet aan zijn en de beker mag niet vol zitten
+                        {
+                            WaterklepOpen;                // openzetten van de waterklep
+                            {
+                                if (flowmeter 300ml)  // Beker zit vol bij 300 ml
+                                {
+                                    WaterklepDicht;   // Waterklep gaat weer dicht
+                                    Tappenklaar = true;  // Aangeven dat de Tap klaar is
+                                }
+                            }
+
+                        }
+
+
+                    }
+                    else (Bekerdoorgeven == true)    //wachten tot dat beker doorgegeven is
+                    {
+                        Delay()
+                    }
+                 }
+                 else (TappenKlaar == false)  // Tap is niet klaar dus mag hij niet door
+                 {
+                      ErrorTappen();
+                 }
+            }
+            else (TapBiertje == true)   // Biertje wordt nog getapt
+            {
+                ErrorBiertjeAanHetTappen();
+            }
+        }
+        else (BekerAanwezig == false)  // Geen beker aanwezig dus er mag niet getapt worden
+        {
+            ErrorBekerafwezig();
+        }
 }
 
 ///////////////////////////////////////////////////////////////////////////////
